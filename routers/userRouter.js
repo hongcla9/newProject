fdc1cd713c77e7a253cb2e68cfb8066f5a47bc80
hongcla9 <hongcla9@gmail.com>
2020-12-14 22:28:53 +0900

    (V view setting rendering)

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,16 @@
 import express from "express";
+import routes from "../routes";
+import {
+  users,
+  userDetail,
+  editProfile,
+  changePassword
+} from "../controllers/userController";
 
-export const userRouter = express.Router();
+const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => res.send("user index"));
-userRouter.get("/edit", (req, res) => res.send("user edit"));
-userRouter.get("/password", (req, res) => res.send("user password"));
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+
+export default userRouter;
