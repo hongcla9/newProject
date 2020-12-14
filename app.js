e1ffe5942a37e73aae8528a0347217ea42f7bb12
hongcla9 <hongcla9@gmail.com>
2020-12-14 22:03:37 +0900

    setting routers

--- app.js
+++ app.js
@@ -3,16 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter  from "./routers/userRouter";
+import videoRouter from "./routers/vIdeoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
-
-const handleHome = (req, res) => res.send("Hello from my ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -20,8 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+app.use("/",globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
-app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
---
+++ init.js
@@ -0,0 +1,13 @@
+import app from "./app";
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`✅ Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
+
+// model - is data v - how does the data look c funciont that loooks for data (MVC)
+
+
+
