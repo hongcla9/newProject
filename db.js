41846f6eff7b74f04d752b848f253e26aa802d25
hongcla9 <hongcla9@gmail.com>
2020-12-15 16:29:16 +0900

    Setting webpack, making styling

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ mongoose.connect(
   process.env.MONGO_URL,
   {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
   }
 );
 
--- init.js
+++ init.js
@@ -2,6 +2,8 @@ import "./db"
 import app from "./app";
 import dotenv from "dotenv";
 import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
